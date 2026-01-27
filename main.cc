3688fc550d5f015bdec5c647beb6f51caf03efe7
Tatakinov <tatakinov@gmail.com>
2026-01-27 22:00:00 +0900

    Windowsでタイトルバーが出現することがあったのを修正した。

--- main.cc
+++ main.cc
@@ -14,6 +14,7 @@
 int main(int argc, char **argv) {
     SDL_SetHint(SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
     SDL_SetHint(SDL_HINT_IME_IMPLEMENTED_UI, "composition");
+    SDL_SetHint("SDL_BORDERLESS_WINDOWED_STYLE", "0");
     SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, "io.github.tatakinov.ninix-kagari.ai.ai_builtin");
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         return 1;
